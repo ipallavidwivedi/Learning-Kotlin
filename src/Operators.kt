7e5f2ac7ff148c07ef1da93965832f54fdb68c87
Pallavi Dwivedi <dpallavi854@gmail.com>
2025-01-21 22:56:46 +0530

    learned arithmetic operators in kotlin

---
+++ src/Operators.kt
@@ -0,0 +1,9 @@
+// learning about arithmetic operators
+fun main(){
+    var num1: Int = 12
+    var num2: Int = 23
+    println("$num1 + $num2 = ${num1 + num2}")
+    println("$num1 - $num2 = ${num1 - num2}")
+    println("$num1 * $num2 = ${num1 * num2}")
+    println("$num1 / $num2 = ${num1 / num2}")
+}
