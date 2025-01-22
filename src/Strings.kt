71b350a28609da4d4e8dc71bb53233401f69514b
Pallavi Dwivedi <dpallavi854@gmail.com>
2025-01-22 22:56:51 +0530

    learned about logical and comparison operators

--- src/Strings.kt
+++ src/Strings.kt
@@ -1,9 +1,18 @@
 //learning about strings
 fun main(){
    var str: String = "I am a girl"
+    //Basic string functions
    println("${str.toLowerCase()}")
    println("${str.toUpperCase()}")
     println("${str.subSequence(2,6)}")
     println("${str.equals("Pallavi")}")
+
+    val x = 9
+    val y = 3
+    val z = 9
+    val bool = true
+    println(x == y) //comparing values of two variables
+    println(!(x != z) && bool || z > (x + y) && (!bool || y < z))
+
 
 }
