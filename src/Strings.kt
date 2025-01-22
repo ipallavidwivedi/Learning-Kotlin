a1e1087a4d09b1a02b0113bcd799de13858ff968
Pallavi Dwivedi <dpallavi854@gmail.com>
2025-01-22 22:45:07 +0530

    learned about strings

---
+++ src/Strings.kt
@@ -0,0 +1,9 @@
+//learning about strings
+fun main(){
+   var str: String = "I am a girl"
+   println("${str.toLowerCase()}")
+   println("${str.toUpperCase()}")
+    println("${str.subSequence(2,6)}")
+    println("${str.equals("Pallavi")}")
+
+}
